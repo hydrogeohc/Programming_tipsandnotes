acf67bd90013419df6100fa51005405db0c58403
Your Name <you@example.com>
2022-01-18 23:44:02 -0800

    Jan-18th

---
+++ decapitalize.py
@@ -0,0 +1,8 @@
+## Decapitalizes the first letter of a string.
+## Decapitalizes the first letter of the string and then adds it with rest of the string. Omit the upper_rest parameter to keep the rest of the string intact, or set it to True to convert to uppercase.
+def decapitalize(string, upper_rest=False):
+    return str[:1].lower() + (str[1:].upper() if upper_rest else str[1:])
+
+
+## decapitalize('FooBar') # 'fooBar'
+## decapitalize('FooBar', True)  # 'fOOBAR'
